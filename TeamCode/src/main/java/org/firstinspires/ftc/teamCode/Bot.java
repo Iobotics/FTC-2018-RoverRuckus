a0ca064f4b3b599ba9e4063e63e5cfd4da782550
Pidud <TsuchitoriJoel@gmail.com>
2018-09-05 17:35:12 -1000

    made module for team 8898

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamCode/Bot.java
@@ -0,0 +1,85 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamCode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Bot {
+
+    final static int ENCODER_TICKS_PER_REV = 1120;
+    final static int WHEEL_DIAMETER = 4; //Inches
+    final static double INCHES_PER_TICK = (WHEEL_DIAMETER * Math.PI) / ENCODER_TICKS_PER_REV;
+
+    int _leftOffset;
+    int _rightOffset;
+
+    private DcMotor leftBackDrive = null;
+    private DcMotor leftFrontDrive = null;
+    private DcMotor rightFrontDrive  = null;
+    private DcMotor rightBackDrive = null;
+    private LinearOpMode opMode = null;
+    private HardwareMap hwMap = null;
+
+    public Bot (LinearOpMode opMode){this.opMode = opMode;}
+
+    public void init (HardwareMap ahwMap) {
+        hwMap = ahwMap;
+
+        leftBackDrive = hwMap.get(DcMotor.class, "backLeft");
+        leftFrontDrive = hwMap.get(DcMotor.class, "frontLeft");
+        rightBackDrive = hwMap.get(DcMotor.class, "backRight");
+        rightFrontDrive = hwMap.get(DcMotor.class, "frontRight");
+        setLeftDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public void setPower(double leftPower, double rightPower){
+        leftBackDrive.setPower(leftPower);
+        leftFrontDrive.setPower(leftPower);
+        rightBackDrive.setPower(rightPower);
+        rightFrontDrive.setPower(rightPower);
+    }
+
+    public void setLeftDirection(DcMotor.Direction direction){
+        leftBackDrive.setDirection(direction);
+        leftFrontDrive.setDirection(direction);
+    }
+
+    public void setRightDirection(DcMotor.Direction direction){
+        rightBackDrive.setDirection(direction);
+        rightFrontDrive.setDirection(direction);
+    }
+}
