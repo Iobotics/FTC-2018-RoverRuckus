2f4671b59f739e56420f6cf28e2f643646dae696
Pidud <TsuchitoriJoel@gmail.com>
2018-09-08 12:35:33 -1000

    added Modules for each team

---
+++ Team8740/src/main/java/org/firstinspires/ftc/team8740/Teleop.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.team8898;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * Created by Robotics on 8/29/2018.
+ */
+@TeleOp(name= "Teleop", group="Bot")
+
+public class Teleop extends LinearOpMode {
+    private Bot robot = new Bot(this);
+
+
+    public void runOpMode(){
+        robot.init(hardwareMap);
+        robot.setPower(gamepad1.left_stick_y, gamepad1.right_stick_y);
+    }
+}
