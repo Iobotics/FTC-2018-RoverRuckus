f80a85e0c78e8aeab20d828185f61a2fc6d9c42f
Pidud <TsuchitoriJoel@gmail.com>
2018-09-12 17:18:44 -1000

    Adjusted Braidens Auto

--- Team8741/src/main/java/org/firstinspires/ftc/team8741/Autonomous.java
+++ Team8741/src/main/java/org/firstinspires/ftc/team8741/Autonomous.java
@@ -1,20 +1,35 @@
 package org.firstinspires.ftc.team8741;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-/**
- * Created by Robotics on 8/29/2018.
- */
-@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name= "Autonomous", group="Bot")
-
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="Autonomous", group = "Bot")
+//@Disabled
 public class Autonomous extends LinearOpMode {
+
     private Bot robot = new Bot(this);
 
-
-    public void runOpMode(){
+    @Override
+    public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
-        robot.turn(.5, 180);
-        robot.turn(.5, -90);
+
+        waitForStart();
+        robot.driveStraight(43);
+        robot.gyroTurn(0.4 ,90);
+        robot.driveStraight(54);
+        robot.gyroTurn(0.4,180);
+        robot.driveStraight(54);
+        robot.gyroTurn(0.4,-134);
+        robot.driveStraight(54);
+        robot.gyroTurn(0.4,-89);
+        robot.driveStraight(108);
+        robot.gyroTurn(0.4,-44);
+        robot.driveStraight(54);
+        robot.gyroTurn(0.4,1);
+        robot.driveStraight(52);
+        robot.gyroTurn(0.4,90);
+        robot.driveStraight(52);
+        robot.gyroTurn(0.4,180);
+        robot.driveStraight(43);
+
     }
 }
