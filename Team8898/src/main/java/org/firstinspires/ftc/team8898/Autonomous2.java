5c95e61fb5667d48526e4b22e49c6955495d906f
Joshua <jmt2301@iolani.org>
2018-10-27 10:23:07 -1000

    Fully updated team 8898 code

---
+++ Team8898/src/main/java/org/firstinspires/ftc/team8898/Autonomous2.java
@@ -0,0 +1,64 @@
+package org.firstinspires.ftc.team8898;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="Autonomous2", group = "Bot")
+//@Disabled
+public class Autonomous2 extends LinearOpMode {
+
+    private Bot robot = new Bot(this);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+        robot.setDropper(.50);
+        waitForStart();
+        //\robot.dropForPosition(3, -.7);
+        // Moving
+        robot.driveStraight(2);
+        robot.gyroTurn(-45);
+        robot.driveStraight(27);
+        robot.gyroTurn(45);
+        robot.driveStraight(27);
+        robot.gyroTurn(-181);
+        // clAIm
+
+        robot.setDropper(.12);
+        try {
+            Thread.sleep(2000);
+        }
+        catch (Exception e){}
+        // Sampling
+        //if (robot.isYellow()){
+
+        //}
+   /*     else{
+            robot.gyroTurn(-225);
+            robot.driveStraight(8);
+            robot.gyroTurn(-160);
+            if (robot.isYellow()){
+
+            }
+            else{
+                    robot.gyroTurn(-225);
+                    robot.driveStraight(8);
+                    robot.gyroTurn(-160);
+                   if (robot.isYellow()){
+
+                   }
+                    else{
+                        robot.gyroTurn(90);
+                    }
+            }
+        }*/
+        //robot.upElevator(.7, 10);
+        //robot.setIntakePower(-.8);
+        //wait(1000);
+        //robot.setIntakePower(0);
+
+        // Parking
+        robot.driveStraight(24);
+        robot.gyroTurn(-223);
+        robot.driveStraight(-80);
+    }
+}
