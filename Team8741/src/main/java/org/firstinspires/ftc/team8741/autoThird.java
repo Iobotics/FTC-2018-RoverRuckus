1fac64bd619eac0c23a524cd95fd055610b9260b
Pidud <TsuchitoriJoel@gmail.com>
2018-10-29 16:06:33 -1000

    seperate class for practices

---
+++ Team8741/src/main/java/org/firstinspires/ftc/team8741/autoThird.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.team8741;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="autoThird", group = "Bot")
+//@Disabled
+public class autoThird extends LinearOpMode {
+
+    private Bot robot = new Bot(this);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+
+        waitForStart();
+        robot.driveStraight(-15);
+        robot.gyroTurn(-90);
+        robot.driveStraight(35);
+        robot.gyroTurn(-45);
+        robot.driveStraight(55);
+        robot.setServo(0);
+        robot.gyroTurn(45);
+        robot.driveStraight(108);
+    }
+}
