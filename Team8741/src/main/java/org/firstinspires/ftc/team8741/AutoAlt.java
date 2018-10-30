319d47522c5d5701a72ada5c40c0adcdb5afa4bd
Pidud <TsuchitoriJoel@gmail.com>
2018-10-29 16:59:02 -1000

    putting back colorsensor code

--- Team8741/src/main/java/org/firstinspires/ftc/team8741/AutoAlt.java
+++ Team8741/src/main/java/org/firstinspires/ftc/team8741/AutoAlt.java
@@ -27,5 +27,35 @@ AutoAlt extends LinearOpMode {
         robot.gyroTurn(45);
         robot.driveStraight(108);
 
+
+     /*waitForStart();
+        robot.setLiftPower(-1);
+        robot.driveStraight(-10);
+        robot.gyroTurn(-90);
+        robot.driveStraight(-6);
+        robot.driveStraight(3);
+        if (robot.isYellow()) {
+            robot.gyroTurn(-135);
+            robot.driveStraight(15);
+            robot.setServo(0);
+            robot.gyroTurn(0);
+            robot.driveStraight(108);
+        }
+        robot.driveStraight(3);
+        if (robot.isYellow()) {
+            robot.gyroTurn(-150);
+            robot.driveStraight(12);
+            robot.setServo(0);
+            robot.gyroTurn(60);
+            robot.driveStraight(108);
+        }
+        robot.driveStraight(3);
+        if (robot.isYellow()) {
+            robot.gyroTurn(-170);
+            robot.driveStraight(10);
+            robot.setServo(0);
+            robot.gyroTurn(80);
+            robot.driveStraight(108);*/
+
+        }
     }
-}
