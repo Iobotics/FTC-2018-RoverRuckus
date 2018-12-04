1dcde01cd57e2ad39a5ce9c33c7a836f4b7bba02
Pidud <TsuchitoriJoel@gmail.com>
2018-12-03 17:22:18 -1000

    added setup for Tensor flow in 8741

--- Team8741/src/main/java/org/firstinspires/ftc/team8741/Auto.java
+++ Team8741/src/main/java/org/firstinspires/ftc/team8741/Auto.java
@@ -19,16 +19,19 @@ public class Auto extends LinearOpMode {
         robot.setLiftPower(1);
         robot.setPower(-0.10, -0.10);
         robot.sleep(4200);
-        robot.setLiftPower(0);  
+        robot.setLiftPower(0);
         robot.gyroTurn(0);
         robot.driveStraight(-19);
+        robot.setLiftPower(-1);
+        robot.sleep(4200);
+        robot.setLiftPower(0);
         robot.gyroTurn(90);
         robot.driveStraight(-46);
         robot.gyroTurn(135);
         robot.driveStraight(-49);
         robot.setServo(0);
         robot.sleep(500);
-        robot.driveStraight(100);
+        robot.driveStraight(96);
 
 
 
