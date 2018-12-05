ee6380eb46ceb0ba6c050a63a5093e8e25afcb6e
ShaneMatsushima <skm1901@iolani.org>
2018-12-04 17:01:18 -1000

    fixed errors in merge

---
+++ Team15076/src/main/java/org/firstinspires/ftc/team15076/Auto.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.team15076;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="AutoDepotBlue", group = "Bot")
+//@Disabled
+public class Auto extends LinearOpMode {
+
+    public Bot robot = new Bot(this);
+
+    @Override
+    public void runOpMode() {
+        /*
+        Auto For the Closest To the Crater
+         */
+
+        robot.init(hardwareMap);
+
+        waitForStart();
+        robot.setLiftPower(1);
+        robot.setPower(-0.5, -0.5, -0.5, -0.5);
+        robot.sleep(850);
+        robot.setLiftPower(0);
+        robot.encoderDrive(-10,1);
+        robot.gyroTurn(-90);
+        robot.encoderDrive(18,1);
+        robot.gyroTurn(330);
+        robot.encoderDrive(24,1);
+        robot.encoderDrive(-30,1);
+        robot.gyroTurn(180);
+        robot.setExtPower(1);
+        robot.sleep(1000);
+        robot.setExtPower(0);
+
+        }
+
+
+
+
+    }
+
