9313e0ad22c638dbfd9070255a8def7460788567
ewh1901 <ewh1901@iolani.org>
2018-12-05 15:09:37 -1000

    Added sampling function to Bot class

--- Team15076/src/main/java/org/firstinspires/ftc/team15076/Bot.java
+++ Team15076/src/main/java/org/firstinspires/ftc/team15076/Bot.java
@@ -589,15 +589,10 @@ public class Bot {
         intake.setPower(speed);
     }
 
-/*
-    public void markerdrop() {
-        this.setPowerDropper(1);
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e1) {}
-        dropperservo.setPower(0);
-    }
->>>>>>> merge
+    public Sampling sample()
+    {
+
+        initVuforia();
 
         if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
             initTfod();
@@ -605,7 +600,63 @@ public class Bot {
             opMode.telemetry.addData("Sorry!", "This device is not compatible with TFOD");
         }
 
-*/
+        /** Wait for the game to begin */
+        opMode.telemetry.addData(">", "Press Play to start tracking");
+        opMode.telemetry.update();
+        opMode.waitForStart();
+
+        if (opMode.opModeIsActive()) {
+            /** Activate Tensor Flow Object Detection. */
+            if (tfod != null) {
+                tfod.activate();
+            }
+
+            double timeOffset = time.time();
+            while (time.time() - timeOffset <= 3000) {
+                if (tfod != null) {
+                    // getUpdatedRecognitions() will return null if no new information is available since
+                    // the last time that call was made.
+                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                        opMode.telemetry.addData("# Object Detected", updatedRecognitions.size());
+                        if (updatedRecognitions.size() == 3) {
+                            int goldMineralX = -1;
+                            int silverMineral1X = -1;
+                            int silverMineral2X = -1;
+                            for (Recognition recognition : updatedRecognitions) {
+                                if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
+                                    goldMineralX = (int) recognition.getLeft();
+                                } else if (silverMineral1X == -1) {
+                                    silverMineral1X = (int) recognition.getLeft();
+                                } else {
+                                    silverMineral2X = (int) recognition.getLeft();
+                                }
+                            }
+                            if (goldMineralX != -1 && silverMineral1X != -1 && silverMineral2X != -1) {
+                                if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) {
+                                    opMode.telemetry.addData("Gold Mineral Position", "Left");
+                                    return Sampling.LEFT;
+                                } else if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) {
+                                    opMode.telemetry.addData("Gold Mineral Position", "Right");
+                                    return Sampling.RIGHT;
+                                } else {
+                                    opMode.telemetry.addData("Gold Mineral Position", "Center");
+                                    return Sampling.CENTER;
+                                }
+                            }
+                        }
+                        opMode.telemetry.update();
+                    }
+                }
+            }
+        }
+
+        if (tfod != null) {
+            tfod.shutdown();
+        }
+
+        return Sampling.CENTER;
+    }
 
 
     /**
@@ -638,11 +689,4 @@ public class Bot {
         tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL);
 
     }
-
-    /*private void setIntakeArm(int position)
-    {
-        intakeServo.setPosition(position);
-    }*/
-
-
 }
